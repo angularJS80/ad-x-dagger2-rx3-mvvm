7a1cd4fe416ea5b43bfc03cf7d118a710bba2191
admin <admin@adminui-MacBook-Pro.local>
2019-03-10 18:14:31 +0900

    Initial commit

---
+++ app/src/androidTest/java/msa/client/androidx_dagger2_mvvm_rx3/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package msa.client.androidx_dagger2_mvvm_rx3
+
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("msa.client.androidx_dagger2_mvvm_rx3", appContext.packageName)
+    }
+}
